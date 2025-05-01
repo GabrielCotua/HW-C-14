2d50d291d4e352751e5ce2a8c16dd9565d034f62
GabrielCotua <46660187+GabrielCotua@users.noreply.github.com>
2025-05-01 16:56:55 -0500

    making menu

--- HW14-2.c
+++ HW14-2.c
@@ -12,7 +12,7 @@
 typedef struct Book {
   char Title[STRSIZE];
   char Author[STRSIZE];
-  int year;
+  int Year;
 } BOOK;
 
 typedef struct Node {
@@ -20,21 +20,26 @@ typedef struct Node {
   struct Node * Next;
 } NODE;
 
-int AppendNode(NODE * head, BOOK value);
+int AppendNode(NODE ** head, BOOK value);
 int FreeList(NODE * head);
+int DisplayList(NODE * head);
 
-int GettingFileInfo(NODE * node, BOOK * book);
+int GettingFileInfo(NODE ** node, BOOK * book);
 
 int main(void) {
   
   NODE * head = NULL;
-  BOOK book = {"HA","HA",2};
-  AppendNode(head, book); 
+  BOOK * book;
+  // MENU
+  printf("***MENU***""
+		  ");
+  GettingFileInfo(&head, book); 
+  DisplayList(head); 
   
   return 0;
 }
 
-int AppendNode(NODE * head, BOOK value) {
+int AppendNode(NODE ** head, BOOK  value) {
 
   NODE * temp = (NODE*) malloc(sizeof(NODE));
   if (!temp) {
@@ -45,23 +50,83 @@ int AppendNode(NODE * head, BOOK value) {
   temp->Next = NULL;
   temp->Item = value;
 
-  if (head == NULL) {
-    head = temp;
+  if (*head == NULL) {
+    *head = temp;
   }
   else {
-    while (temp->Next != NULL) {
-      temp = temp->Next;
+	  NODE * current = *head;
+    while (current->Next != NULL) {
+      current = current->Next;
     }
+    current->Next = temp;
   }
-  printf("List loaded successfully");
+  printf("List loaded successfully\n");
 
-  free(temp);
   return 0;
 }
 
 int FreeList(NODE * head) {
-  
+  NODE * temp;
+  while (head != NULL) {
+    temp = head;
+    head = head->Next;
+    free(temp);
+  } 
   return 0;
 }
 
-int GettingFileInfo(NODE * node, BOOK * book);
+int GettingFileInfo(NODE ** node, BOOK * book) {
+
+  char title[STRSIZE];
+  char author[STRSIZE];
+  char yearStr[10];
+  int year;
+  FILE * fp = fopen(FILENAME, "r");
+  
+  if (!fp) {
+    printf("Error Opening File %s\n", FILENAME);
+    fclose(fp);
+    return -2;
+  }
+  
+  while(fgets(title, STRSIZE, fp)) {
+    fgets(author, STRSIZE, fp);
+    fgets(yearStr, 10,fp);
+
+    title[strcspn(title,"\n")] = '\0';
+    author[strcspn(author, "\n")] = '\0';
+    yearStr[strcspn(yearStr, "\n")] = '\0';
+
+    year = atoi(yearStr);
+
+    BOOK * tempBook = (BOOK*) malloc(sizeof(BOOK));
+
+    if(!tempBook) {
+      printf("Memory allocation failed! error: -3\n");
+      fclose(fp);
+      return -3;
+    }
+
+    strcpy(tempBook->Title, title);
+    strcpy(tempBook->Author, author);
+
+    tempBook->Year = year;
+    AppendNode(node,* tempBook);
+  } 
+  
+  return 0;
+
+}
+
+int DisplayList(NODE * head) {
+  NODE * tempNode = head; 
+  printf("Opening DisplayListLOOP\n");
+  while (tempNode != NULL) {
+    printf("Title: %s\n", tempNode->Item.Title);
+    printf("Author: %s\n", tempNode->Item.Author);
+    printf("Year: %d\n", tempNode->Item.Year);
+    tempNode = tempNode->Next; 
+    putchar('\n');
+  }
+  return 0;
+}
